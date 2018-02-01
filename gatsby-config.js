fa6be4032d0d4ba40bd79c16f81bdc77616d4511
Khaled Garbaya <khaledgarbaya@gmail.com>
2018-02-01 14:03:12 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,8 +6,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: process.env.KHALED_GARBAYA_CF_SPACE || '3bc97k4uk5q7',
-        accessToken: process.env.KHALED_GARBAYA_CF_TOKEN || 'cad8217f92693dac2350540d49d51dd459a8c9a9c2729dcc90409a96e197cb48'
+        spaceId: process.env.CF_SPACE || '3bc97k4uk5q7',
+        accessToken: process.env.CF_TOKEN || 'cad8217f92693dac2350540d49d51dd459a8c9a9c2729dcc90409a96e197cb48'
       }
     },
     `gatsby-transformer-remark`,
